2ed1a5ddb766e9ed7a32afd952f2bc401c8a4742
Elisha Tam <elisha.tam@gmail.com>
2015-07-19 11:05:52 -0700

    Updated code so not blocking code. Used port manipulation

--- capacitiveTouchRelay/_2015_07_08_test1/_2015_07_08_test1.ino
+++ capacitiveTouchRelay/_2015_07_08_test1/_2015_07_08_test1.ino
@@ -1,6 +1,9 @@
 #include <CapacitiveSensor.h>
 /*Elisha's time spent:
-
+ - 3 hrs researching different sensors - capacitive, pressure,
+ - 4 hrs trying to get wireless way to activate capacitive sensor
+ - 1 hr soldering and figuring
+ - 10:30am - timing
 */
 
 /*
@@ -20,9 +23,14 @@ CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10 megohm resistor 
 //CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);        // 10 megohm resistor between pins 4 & 6, pin 6 is sensor pin, add wire, foil
 //CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10 megohm resistor between pins 4 & 8, pin 8 is sensor pin, add wire, foil
 int relayPin = 7;
+unsigned long keyPrevMillis = 0;
+const unsigned long keySampleIntervalMs = 100;
 
 void setup()                    
 {
+  //Debug
+  DDRB = DDRB | B00100000; //Set Arduino pin 13 (LED) to output, rest to input
+  //
    pinMode(relayPin, OUTPUT);
    cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
@@ -32,7 +40,26 @@ void setup()
 
 void loop()                    
 {
-  
+    //Check capacitive sensor every 100ms (keySampleIntervalMs)
+    if (millis() - keyPrevMillis >= keySampleIntervalMs){
+      keyPrevMillis = millis();
+      PORTB ^= B00100000; //Toggle LED to test Only toggles Bit 5 
+      long start = millis();
+      long total1 = cs_4_2.capacitiveSensor(30);
+      
+      if (total1 > 200){
+        digitalWrite(relayPin, HIGH); //connect Relay
+      }
+      else digitalWrite(relayPin, LOW);
+    Serial.print(millis() - start);        // check on performance in milliseconds
+    Serial.print("\t");                    // tab character for debug window spacing
+
+    Serial.println(total1);                  // print sensor output 1
+ 
+    }
+    
+    /*
+    Capacitive sensor turns on relay when signal is high. Blocking code, delay for 100ms
     long start = millis();
     long total1 =  cs_4_2.capacitiveSensor(30);
     //long total2 =  cs_4_5.capacitiveSensor(30);
@@ -53,8 +80,11 @@ void loop()
     //Serial.println(total3);                // print sensor output 3
 
     delay(100);                             // arbitrary delay to limit data to serial port 
+   */
+   
    
    /*
+   //Test Relay Pin
    digitalWrite(relayPin, HIGH);
    Serial.println("hi");
    delay(1000);
@@ -63,3 +93,21 @@ void loop()
    delay(1000);
    */
 }
+
+
+/*Port Manipulation
+https://www.arduino.cc/en/Reference/PortManipulation
+//NOTES
+//0000,     0000
+//7 6 13 12, 11 10 9 8
+DDRB = DDRB | B00100000; //Set Arduino pin 13 (LED) to output, rest to input
+void loop() {
+  PORTB = PORTB | B00100000; //Sets digital pin 13 high
+//  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a second
+//  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  PORTB &= ~B00100000; //Sets digital pin 13 low
+  delay(100);               // wait for a second
+}   
+   */
+
