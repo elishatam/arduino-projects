b40f750a6c26e7e32562fe64b229d66a3f680569
Elisha Tam <elisha.tam@gmail.com>
2015-07-29 15:14:36 -0700

    Change code so not blocking

--- MOSFETs/Blink/Blink.ino
+++ MOSFETs/Blink/Blink.ino
@@ -10,6 +10,8 @@
 #define VOCPWMPin 8 //(P2_0)  
 int VOCAnalogPin = A5;
 int VOCVal = 0;
+unsigned long previousMillis = 0;
+unsigned int state = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -21,10 +23,21 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(VOCPWMPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  VOCVal = analogRead(VOCAnalogPin); //read the input pin
-  delay(2);               // wait for a second
-  digitalWrite(VOCPWMPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(998);               // wait for a second
-  Serial.println(VOCVal);
+  unsigned long currentMillis = millis();
+  
+  if(state == 0 && (currentMillis - previousMillis >= 998)){ //After 998ms, make VOCPWMPin high for 2ms
+    digitalWrite(VOCPWMPin, HIGH);   // turn VOCPWMPin on for 2ms
+    state = 1;
+    VOCVal = analogRead(VOCAnalogPin); //read the input pin
+    previousMillis = currentMillis;
+    Serial.println(VOCVal);  
+  }
+
+  if(state == 1 && (currentMillis - previousMillis >= 2)){ //After 2ms, make VOCPWMPin low for 998
+    digitalWrite(VOCPWMPin, LOW); //turn VOCPWMPin off
+    state = 0;
+    previousMillis = currentMillis;
+  }
+    
+  
 }
