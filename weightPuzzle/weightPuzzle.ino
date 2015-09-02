151b3e0b469f198554995edabf3a72a8cb72ef35
Elisha <elisha.tam@gmail.com>
2015-09-02 08:57:56 -0700

    Begin switch statement. Push button, set TargetWeight and turn on LED.
    Edited the TARE function so I can hardcode the OFFSET so the device doesn't have to be empty to begin reading correctly

--- weightPuzzle/weightPuzzle.ino
+++ weightPuzzle/weightPuzzle.ino
@@ -5,6 +5,11 @@
 //Sun, 8/30. 1hr - set up test setup
 //Tues, 9/1. 1hr- Solder pins on HX711 and crimp pins to wire
 
+typedef enum { FIRSTSTATE,
+               SETTARGETWEIGHT,
+               CHECKWEIGHT
+} STATE_t;
+
 #define calibration_factor 210000.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
 #define DOUT 3 //Define pin number for DOUT of weight sensor
 #define CLK 2  //Define pin number for CLK of weight sensor
@@ -14,19 +19,57 @@ const int ledPin = 6;
 
 //variables will change
 int buttonState = 0; 
+int lastButtonState = 0;
 
 HX711 scale(DOUT, CLK);
+STATE_t myState; //Current state
+STATE_t dummyState; //Used to prevent switching states within one while loop iteration
 
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 
+  //Read the state of the last pushbutton
+  lastButtonState = digitalRead(buttonPin);
   scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+  myState = FIRSTSTATE;
 }
 
 void loop() {
+  float targetWeight, weight;
+  
+  dummyState = myState;
+
+  switch (dummyState){
+    case FIRSTSTATE:
+      //Read the state of the pushbutton
+      buttonState = digitalRead(buttonPin);  
+      Serial.print(scale.get_offset());
+      Serial.print("\t");
+      Serial.println(buttonState);
+      if (buttonState == 0){ //When pressed, buttonState = 0
+        myState = SETTARGETWEIGHT;
+      }
+      break;
+
+    case SETTARGETWEIGHT:
+      digitalWrite(ledPin, HIGH);
+      weight = scale.get_units(); //in lbs
+      Serial.println(weight, 2);
+      if (buttonState == 0){
+        targetWeight = weight;
+      }
+      break;
+      
+  }
+  
+  
+  
+  
+  
+
 /*  //read the state of the pushbutton
   buttonState = digitalRead(buttonPin);
   if (buttonState == 0){
@@ -39,10 +82,11 @@ void loop() {
   
   delay(10);
 */
-  Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 2); //scale.get_units() returns a float
-  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
-  Serial.println();
-  
+/*
+  float weight;
+  weight = scale.get_units(); //in lbs
+  Serial.println(weight, 2);
+*/
+ 
 
 }
