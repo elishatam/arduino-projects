8ecd112576dec31b8e281d275e8149d31908b220
Elisha <elisha.tam@gmail.com>
2015-09-29 08:16:11 -0700

    Change to Switch Statement and change functionality so unlock after touching for >10 seconds and lock after 10 seconds has passed
    INITIALIZATION STATE - takes 10sec to get average baseline reading
    WAITINGFORTOUCH - Waiting for touch for 10sec
    UNLOCK - unlocked state. Locks after 10sec

--- capacitiveTouchRelay/_2015_07_08_test1/_2015_07_08_test1.ino
+++ capacitiveTouchRelay/_2015_07_08_test1/_2015_07_08_test1.ino
@@ -1,4 +1,6 @@
 #include <CapacitiveSensor.h>
+
+#include <CapacitiveSensor.h>
 /*Elisha's time spent:
  - 3 hrs researching different sensors - capacitive, pressure,
  - 4 hrs trying to get wireless way to activate capacitive sensor
@@ -24,11 +26,20 @@
  //     - After initialization, HIGH = LOCK 
  //     - After touching for 10sec, LOW = UNLOCK
 
+typedef enum { INITIALIZATIONSTATE,
+               WAITINGFORTOUCH,
+               UNLOCK
+} STATE_t;
+
+
+
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
 //CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);        // 10 megohm resistor between pins 4 & 6, pin 6 is sensor pin, add wire, foil
 //CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10 megohm resistor between pins 4 & 8, pin 8 is sensor pin, add wire, foil
 int relayPin = 7;
+int ledPin = 13;
 unsigned long keyPrevMillis = 0;
+unsigned long unlockPrevMillis = 0;
 const unsigned long keySampleIntervalMs = 100;
 const unsigned long triggerCapacitanceAmount = 50;
 unsigned int triggerAmount;
@@ -46,6 +57,10 @@ unsigned int initializationPeriod = 0;
 
 unsigned int prevKeyState = 0; //to start off, capacitive sensor state is 0, <200.
 
+STATE_t myState; //Current state
+STATE_t dummyState; //Used to prevent switching states within one while loop iteration
+
+
 void setup()                    
 {
   //Debug
@@ -57,28 +72,117 @@ void setup()
    pinMode(relayPin, OUTPUT);
    cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
-   digitalWrite(relayPin, HIGH); //set Relay to initially HIGH = lock
+   lock();
+   //digitalWrite(relayPin, HIGH); //set Relay to initially HIGH = lock
+   myState = INITIALIZATIONSTATE;
 
 }
 
 // called when button is kept pressed for less than 2 seconds
-void shortKeyPress() {
-    Serial.println("short");
+void lock() {
+    Serial.println("Locked");
     digitalWrite(relayPin, HIGH); //make sure relay is high = lock
+    digitalWrite(ledPin, HIGH);
 }
 
 // called when button is kept pressed for more than 2 seconds
-void longKeyPress() {
-    Serial.println("long");
+void unlock() {
+    Serial.println("Unlocked the lock");
     digitalWrite(relayPin, LOW); //set Relay to low to unlock and disconnect Relay
+    digitalWrite(ledPin, LOW);
 }
 
 // called when key goes from not pressed to pressed
-void keyPress() {
-    Serial.print("key press");
+void beginTouch() {
+    Serial.print("Begin Touch");
     longKeyPressCount = 0;
 }
 
+
+void loop() {
+  unsigned long currentMillis;
+  currentMillis = millis();
+  dummyState = myState;
+  
+  //Check capacitive sensor every 100ms (keySampleIntervalMs)
+  if (currentMillis - keyPrevMillis >= keySampleIntervalMs){
+    keyPrevMillis = currentMillis;
+
+    switch (dummyState){
+      case INITIALIZATIONSTATE:{
+        //Get average first reading during initialization time
+        //Initialization time = 10s (100 * 100ms = 10,000ms = 10s)
+        if (initializationPeriod < 100){
+            Serial.print("INIT");
+            Serial.print("\t");
+            total = total - firstReadings[index];
+            firstReadings[index] = cs_4_2.capacitiveSensor(30); //read Key state
+            Serial.print(firstReadings[index]);
+            total = total + firstReadings[index];
+            index = index + 1;
+          
+            if (index >= numReadings)
+              index = 0;
+            
+            average = total / numReadings;
+            initializationPeriod++;
+            //firstTimeToRead = 0;
+            //triggerAmount = average + triggerCapacitanceAmount;
+            Serial.print("\t");
+            Serial.println(average);
+        }
+        else {
+            myState = WAITINGFORTOUCH;
+        }
+        break;
+      }
+      case WAITINGFORTOUCH:{
+        //Now we know the average first reading. 
+        //Our triggerAmount will just be triggerCapacitanceAmount (50) above the average reading 
+        //If capacitive reading got above this triggerAmount, then we assume someone is touching the sensor     
+        triggerAmount = average + triggerCapacitanceAmount;
+        long currKeyState = cs_4_2.capacitiveSensor(30); //read Key state
+        if ((prevKeyState < triggerAmount) && (currKeyState > triggerAmount)){
+          beginTouch(); //Relative measurement. Capacitance amount has to be > prevKeyState by triggerCapacitiveAmount in order to trigger.
+        }
+        else if (currKeyState > triggerAmount){
+          longKeyPressCount++;
+        }
+        if (longKeyPressCount >= longKeyPressCountMax){
+          //longKeyPress(); //Unlock
+          myState = UNLOCK;
+          unlockPrevMillis = millis();
+          longKeyPressCount = 0;
+        }
+        
+        prevKeyState = currKeyState;
+        Serial.print("WAITINGFORTOUCH");
+        Serial.print("\t");
+        Serial.print(average);
+        Serial.print("\t");
+        Serial.print(triggerAmount);
+        Serial.print("\t");
+        Serial.print(longKeyPressCount);
+        Serial.print("\t");                    // tab character for debug window spacing
+        Serial.println(currKeyState); 
+        break;
+     }
+     case UNLOCK:{
+        Serial.print("UNLOCK");
+        Serial.print("\t");
+        unlock();  //unlock
+        if (millis() - unlockPrevMillis >= 10000){
+          lock(); //lock
+          myState = WAITINGFORTOUCH;
+          unlockPrevMillis = millis();
+        }
+        break;
+     }
+    }
+  }
+}
+
+/*
 void keyRelease(){
     Serial.println("key release");
     
@@ -90,10 +194,9 @@ void keyRelease(){
     }
   
 }
+*/
 
-void loop()                    
-{
-    
+    /*
     //Check capacitive sensor every 100ms (keySampleIntervalMs)
     if (millis() - keyPrevMillis >= keySampleIntervalMs){
       keyPrevMillis = millis();
@@ -127,11 +230,14 @@ void loop()
         if ((prevKeyState < triggerAmount) && (currKeyState > triggerAmount)){
           keyPress(); //Relative measurement. Capacitance amount has to be > prevKeyState by triggerCapacitiveAmount in order to trigger.
         }
-        else if ((prevKeyState > triggerAmount) && (currKeyState < triggerAmount)){
-          keyRelease();
-        }
+        //else if ((prevKeyState > triggerAmount) && (currKeyState < triggerAmount)){
+        //  keyRelease();
+        //}
         else if (currKeyState > triggerAmount){
           longKeyPressCount++;
+        }
+        if (longKeyPressCount >= longKeyPressCountMax){
+          longKeyPress(); //Unlock
         }
         
         prevKeyState = currKeyState;
@@ -147,7 +253,7 @@ void loop()
       
       
     }
-      
+    */  
 /*      
       long start = millis();
       long total1 = cs_4_2.capacitiveSensor(30);
@@ -197,7 +303,7 @@ void loop()
    Serial.println("low");
    delay(1000);
    */
-}
+//}
 
 
 /*Port Manipulation
